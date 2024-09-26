70c15dce7df9c6a180836f8a2c8718ee22eeb5c0
bia-cabral <beatriz.cabral@germinare.org.br>
2024-09-26 12:58:41 -0300

    Hehe 2

---
+++ app/src/main/java/com/aula/appbimo/FluxoLogin/FotoActivity.java
@@ -0,0 +1,16 @@
+package com.aula.appbimo.FluxoLogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aula.appbimo.R;
+
+public class FotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foto);
+    }
+}
